5eee2c3873108e0344fb1cf3e6fdbf2bbd5f2d4e
Albina-K <kamalieva.albish@mail.ru>
2024-04-20 18:27:05 +0300

    Добавьте файлы проекта.

---
+++ Module10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface ILogger
+    {
+        void Write () { Console.WriteLine("Hello! I am Logger!"); }
+    }
+
+    public interface IWriter
+    {
+        void Write() { Console.WriteLine("Hello! I am Writer!"); }
+    }
+    public class BuisnessLogic : IWriter, ILogger
+    {
+
+    }
+}
